f696ab745b927760b24a5bd00f0319a923a74cb8
NilsElveros <95762058+NilsElveros@users.noreply.github.com>
2025-10-10 10:42:50 +0200

    feat: add parent project processer with subproject vard-av-husdjur
    * Added the first draft of vard-av-husdjur process
    
    * Fixed outgoing kafka message property

---
+++ processer/vard-av-husdjur/src/main/java/vah/VahRtfResponse.java
@@ -0,0 +1,18 @@
+package vah;
+
+import java.util.UUID;
+
+public class VahRtfResponse {
+
+    public UUID processId;
+    public boolean result;
+
+    public VahRtfResponse() {
+    }
+
+    public VahRtfResponse(UUID processId, boolean result) {
+        this.processId = processId;
+        this.result = result;
+    }
+
+}
