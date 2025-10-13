fa80921755ff8ca56a6da4a6cb3f6e10ce26f2de
Ulf Slunga <ulf.slunga@sinetiq.se>
2025-10-13 14:42:24 +0200

    Removed not required annotations

--- folkbokford/src/main/java/fk/rimfrost/PopulationRegistration.java
+++ folkbokford/src/main/java/fk/rimfrost/PopulationRegistration.java
@@ -1,6 +1,7 @@
 package fk.rimfrost;
 
 import jakarta.ws.rs.GET;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -8,16 +9,15 @@ import jakarta.ws.rs.core.MediaType;
 import se.fk.rimfrost.api.folkbokforing.jaxrsspec.controllers.generatedsource.PopulationRegistrationControllerApi;
 import se.fk.rimfrost.api.folkbokforing.jaxrsspec.controllers.generatedsource.model.PopulationRegistrationSocialSecurityNrGet200Response;
 
+@ApplicationScoped
 @Path("/population_registration")
-public class population_registration implements PopulationRegistrationControllerApi
+public class PopulationRegistration implements PopulationRegistrationControllerApi
 {
    @GET
    @Path("/{social_security_nr}")
-   @Produces(MediaType.APPLICATION_JSON)
    public PopulationRegistrationSocialSecurityNrGet200Response populationRegistrationSocialSecurityNrGet(
-         @PathParam("social_security_nr") String social_security_nr)
+         String social_security_nr)
    {
-
       var response = new PopulationRegistrationSocialSecurityNrGet200Response();
       String lastFour = social_security_nr.substring(social_security_nr.length() - 4);
       response.setResult(!lastFour.equals("9999"));
